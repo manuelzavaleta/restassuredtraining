939a03564a11a73426b7f62e3c2721a74d622ad5
manuel.zavaleta <manuel.zavleta@globant.com>
2018-04-03 14:47:59 -0500

    complete exercise 1

--- restassuredtest/src/test/java/com/backend/HitEndPointAndPrintTest.java
+++ restassuredtest/src/test/java/com/backend/HitEndPointAndPrintTest.java
@@ -1,17 +1,31 @@
 package com.backend;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.when;
 
 public class HitEndPointAndPrintTest {
 
-    @Test
-    public void hitEndPointAndPrint() {
+    @DataProvider(name = "PerfectCoffeeAnswers")
+    public static Object[][] perfectCoffeeAnswers() {
+        return new Object[][]{
+                {"posts"},
+                {"comments"},
+                {"albums"},
+                {"photos"},
+                {"todos"},
+                {"users"},
+
+        };
+    }
+
+    @Test(dataProvider = "PerfectCoffeeAnswers")
+    public void hitEndPointAndPrint(String resource) {
         when()
-                .get("http://jsonplaceholder.typicode.com/posts")
-        .then()
+                .get("http://jsonplaceholder.typicode.com/" + resource)
+                .then()
                 .statusCode(200)
                 .extract()
                 .response()
